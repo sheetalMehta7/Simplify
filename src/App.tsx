77cd454a1f7fe31cfb90d5d8c66772e4b2ea73a6
sheetalMehta7 <sheetalmehta137@gmail.com>
2024-09-02 08:17:06 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-slate-800 w-screen h-screen">
+        <button className="bg-slate-500 text-white rounded px-4 py-2 m-10">Tailwind CSS</button>
+      </div>
+    </>
+  )
+}
+
+export default App
